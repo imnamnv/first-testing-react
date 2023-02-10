75a97955e0502f7c65c378421383521d4bb6033e
NamNV <imnamnv@gmail.com>
2023-02-10 14:58:03 +0700

    finish test UserList

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,26 +1,42 @@
 import { render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 
-test("render one row per user", () => {
-  // Render the component
+function renderComponent() {
   const users = [
     { name: "jane", email: "jane@jane.com" },
     { name: "sam", email: "sam@sam.com" },
   ];
+  // React Testing library recomment we not render component in beforeEach
+  render(<UserList users={users} />);
+  return { users };
+}
 
-  // Way 1:
-  //   render(<UserList users={users} />);
+test("render one row per user", () => {
+  // Render the component
+  renderComponent();
 
   //   // Find all the rows in the table
-  //   const rows = within(screen.getByTestId("users")).getAllByRole("row"); //using data-testid
+  const rows = within(screen.getByTestId("users")).getAllByRole("row"); //using data-testid
 
   // Way 2:
-  const { container } = render(<UserList users={users} />); // the root element of component which auto added by react
+  //   const { container } = render(<UserList users={users} />); // the root element of component which auto added by react
 
-  // Find all the rows in the table
-  // eslint-disable-next-line
-  const rows = container.querySelectorAll("tbody tr");
+  //   // Find all the rows in the table
+  //   // eslint-disable-next-line
+  //   const rows = container.querySelectorAll("tbody tr");
 
   // Assertion: correct number of rows in the table
   expect(rows).toHaveLength(2);
+});
+
+test("render the email and name of each user", () => {
+  const { users } = renderComponent();
+
+  for (let user of users) {
+    const name = screen.getByRole("cell", { name: user.name });
+    const email = screen.getByRole("cell", { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
 });
